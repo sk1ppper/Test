bc6fa903ce99491d5e9f5e49ba4b813ea504a6c6
sk1ppper <muraturazbaev@icloud.com>
2019-04-03 22:45:12 +0300

    init commit

---
+++ src/main/java/com/example/Test/TestApplication.java
@@ -0,0 +1,13 @@
+package com.example.Test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestApplication.class, args);
+	}
+		//2mj2kj2nk2njk2nkj2njk2
+}
